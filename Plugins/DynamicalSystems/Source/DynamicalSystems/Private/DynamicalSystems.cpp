8497de1e9c99b68e518e5cc2ed7c77c3455104ae
Raúl Pleitez <racsoraul@gmail.com>
2019-05-18 08:26:29 -0600

    Adds DynamicialSystems plugin.

---
+++ Plugins/DynamicalSystems/Source/DynamicalSystems/Private/DynamicalSystems.cpp
@@ -0,0 +1,17 @@
+#include "DynamicalSystems.h"
+#include "Core.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "FDynamicalSystemsModule"
+
+void FDynamicalSystemsModule::StartupModule()
+{
+}
+
+void FDynamicalSystemsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FDynamicalSystemsModule, DynamicalSystems)
---
+++ Plugins/DynamicalSystems/Source/DynamicalSystems/Public/DynamicsCommon.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Components/SceneCaptureComponent2D.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "SceneView.h"
+#include "Misc/FileHelper.h"
+#include "DynamicsCommon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNAMICALSYSTEMS_API UDynamicsCommon : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+  public:
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float MeanOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float VarianceOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float MedianOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Float")
+    static float StandardDeviationOfFloatArray(const TArray<float> &Samples);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Rotator")
+    static FQuat RotatorToQuat(FRotator Rotator);
+
+    UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Math|Vector")
+    static FVector CubicBezier(float Time, FVector P0, FVector P1, FVector P2, FVector P3);
+};

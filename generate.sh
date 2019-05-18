d384c004c756711b802b0a2d523b4fd850fc249b
Raúl Pleitez <racsoraul@gmail.com>
2019-05-18 12:22:16 -0600

    Adds Pascal VOC generation for multiple objects.

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "Generating UnrealGAMS project files"
+echo "Generating Synthetic project files"
 echo "----------------------"
 "$UE4_ROOT"/GenerateProjectFiles.sh -project="$UE4_SYNTHETIC"/Synthetic.uproject -game -engine -Makefile -vscode
